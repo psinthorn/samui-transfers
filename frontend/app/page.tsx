b9075be40200ad1bba62ca1d2569937c6a64fefc
Sinthorn Pr <sinthornpr@Sinthorns-iMac.local>
2025-08-17 18:46:39 +0700

    update chatbot interface

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -38,8 +38,8 @@ const { destination, setDestination } = useDestinationContext();
               <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-24 gap-0 bg-white">
                 <div className='col-span-1 flex flex-col gap-4 p-8'>
                   
-                  <h1 className='text-bold text-7xl text-primary'>Chat Now</h1>
-                  <p>Need more information? about us and our service</p>
+                  <h1 className='text-bold text-7xl text-primary'>Start Chat</h1>
+                  <p>Chat with us for quick answers about our transfers, pricing, or routes.</p>
                   
                   {/* <SearchSection /> */}
                 </div>
