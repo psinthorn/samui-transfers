631cd1b28fa9496d43d45def57e8298dd35f521d
Sinthorn Pr <sinthornpr@Sinthorns-iMac.local>
2025-08-17 08:23:20 +0700

    update layout

--- frontend/components/hero/MainBanner.tsx
+++ frontend/components/hero/MainBanner.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import Banner from '../../public/samui-transfers-001.jpeg'
+// import Banner from '../../public/samui-transfers-001.jpeg'
 
 const MainBanner = () => {
   return (
     // use max-h-96 if need the search box show in the middle of the banner
     <div className='space-y-4 p-4 hidden lg:block md:w-full md:h-[95%] md:p-6'>
-      <Image src={Banner} alt='samui transfers main banner' />
+      {/* <Image src={Banner} alt='samui transfers main banner' /> */}
     </div>
   )
 }
