a25fd5799f01eb2cffb5614ae44cf4129500b3ac
Sinthorn Pr <sinthornpr@Sinthorns-iMac.local>
2025-08-25 10:23:24 +0700

    rm -r for cleanup project folder

--- backend/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/psinthorn/locoride
-
-go 1.23
-
-require (
-	github.com/go-chi/chi/v5 v5.1.0
-	github.com/go-chi/cors v1.2.1
-)
--- context/GoogleApiKeyContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext  } from "react";
-
-const  GoogleApiKeyContext = createContext(any);
-
-export default GoogleApiKeyContext;
